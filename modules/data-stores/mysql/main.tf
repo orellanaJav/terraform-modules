f37c2f558f7d8065f6faa3bcd2cba10254b174bc
Javier Orellana <javier.orellana2704@gmail.com>
2024-07-08 13:33:11 -0400

    add: Added data-stores module

---
+++ modules/data-stores/mysql/main.tf
@@ -0,0 +1,17 @@
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+
+  # Enable backups
+  backup_retention_period = var.backup_retention_period
+
+  # If specified, this DB will be a replica
+  replicate_source_db = var.replicate_source_db
+
+  db_name  = var.db_name
+  username = var.db_username
+  password = var.db_password
+}
---
+++ modules/data-stores/mysql/outputs.tf
@@ -0,0 +1,4 @@
+output "arn" {
+  value       = aws_db_instance.example.arn
+  description = "The ARN of the database"
+}
