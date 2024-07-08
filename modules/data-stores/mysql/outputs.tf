1bf48832824464beb0aaa5ba9ac973b9b728a3e7
Javier Orellana <javier.orellana2704@gmail.com>
2024-07-08 19:09:13 -0400

    update: Updated data-stores module variables

--- modules/data-stores/mysql/outputs.tf
+++ modules/data-stores/mysql/outputs.tf
@@ -1,4 +1,14 @@
+output "address" {
+  value       = aws_db_instance.example.address
+  description = "The address of the database"
+}
+
 output "arn" {
   value       = aws_db_instance.example.arn
   description = "The ARN of the database"
+}
+
+output "port" {
+  value       = aws_db_instance.example.port
+  description = "The port of the database"
 }
